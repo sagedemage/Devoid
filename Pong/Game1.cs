ecf645eb6b4ccdd7312b04270afb5afca5f7b50f
Salmaan Saeed <salsaeed999@gmail.com>
2023-03-12 00:54:10 -0500

    Add project files.

---
+++ Pong/Game1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    public class Game1 : Game {
+        /* Game */
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1(){
+            /* Create Game Object */
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            /* Initialize the game */
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 100f;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            /* Load your game content */
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Load your game content here
+            ballTexture = Content.Load<Texture2D>("ball");
+        }
+
+        protected override void Update(GameTime gameTime) {
+            /* Game Logic */
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+
+            /* Player Movement */
+            if (kstate.IsKeyDown(Keys.Up)) {
+                // Move the player up
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (kstate.IsKeyDown(Keys.Down)) {
+                // Move the player down
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (kstate.IsKeyDown(Keys.Left)) {
+                // Move the player left
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            if (kstate.IsKeyDown(Keys.Right)) {
+                // Move the player right
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            /* Player Boundaries */
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2) {
+                // Right Boundary
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            }
+            else if (ballPosition.X < ballTexture.Width / 2) {
+                // Left Boundary
+                ballPosition.X = ballTexture.Width / 2;
+            }
+            
+            // Y boundaries
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2) {
+                // Top Boundary
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            }
+            else if (ballPosition.Y < ballTexture.Height / 2) {
+                // Botton Boundary
+                ballPosition.Y = ballTexture.Height / 2;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            /* Draw your content here */
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
