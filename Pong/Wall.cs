fa6a85553b4e8896bb0c8d250c102f04405556a7
Salmaan Saeed <salsaeed999@gmail.com>
2023-03-15 00:44:16 -0400

    added methods to the wall class

--- Pong/Wall.cs
+++ Pong/Wall.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pong
 {
     public class Wall
     {
-        public Texture2D Texture;
+        private Texture2D Texture;
         public Vector2 Position;
 
         public Wall(Vector2 wallPostion)
@@ -21,6 +16,21 @@ namespace Pong
         public void setTexture(Texture2D wallTexture)
         {
             this.Texture = wallTexture;
+        }
+
+        public Texture2D getTexture()
+        {
+            return this.Texture;
+        }
+
+        public int getTextureWidth()
+        {
+            return this.Texture.Width;
+        }
+
+        public int getTextureHeight()
+        {
+            return this.Texture.Height;
         }
     }
 }
