6b218ceb8fa52010b75917fa52d5ae8b990b77d8
Salmaan Saeed <salsaeed999@gmail.com>
2023-04-16 18:06:03 -0400

    build and run unit tests in batch script

--- run.bat
+++ run.bat
@@ -1,2 +1,9 @@
 @ECHO OFF
+:: Build the Program
+dotnet build --configuration Release --no-restore
+
+:: Test the Program
+dotnet test --no-restore --verbosity normal
+
+:: Run the Program
 dotnet run --project Devoid
