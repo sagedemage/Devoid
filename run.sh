78704e5e1bab38c2e23f0dece69a9f286dc76cab
Salmaan Saeed <salsaeed999@gmail.com>
2023-04-16 18:18:51 -0400

    run unit tests while running the linux shell script

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# Build the Program
+dotnet build --configuration Release --no-restore
+
+# Test the Program
+dotnet test --no-restore --verbosity normal
+
+# Run the Program
 dotnet run --project Devoid
